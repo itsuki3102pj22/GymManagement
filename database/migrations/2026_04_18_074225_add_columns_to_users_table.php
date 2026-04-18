a74f0c6a97d76668c8bd2714b149040bd0557e16
itsuki3102pj22 <i.matsu3102@gmail.com>
2026-04-18 16:44:09 +0900

    issue#1_再設定

--- database/migrations/2026_04_18_074225_add_columns_to_users_table.php
+++ database/migrations/2026_04_18_074225_add_columns_to_users_table.php
@@ -6,26 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->tinyInteger('role')->default(1); // 1:Trainer, 2:Supervisor
-            $table->string('stripe_id')->nullable();
-            $table->string('remember_token', 100)->nullable();
+            $table->tinyInteger('role')->default(1)->after('password');
+            // 1: Trainer, 2: Supervisor
+            $table->string('stripe_id')->nullable()->after('role');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //　ロールカラム削除
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['role']);
+            $table->dropColumn(['role', 'stripe_id']);
         });
     }
 };
